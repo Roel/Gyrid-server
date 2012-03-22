e1be9743035d6dcafe6b08a6af8a5b29eb6e408d
Roel Huybrechts <mail@rulus.be>
2012-03-22 12:07:06 +0100

    Update docstrings.

--- olof/tools.py
+++ olof/tools.py
@@ -5,12 +5,19 @@
 # Copyright (C) 2011-2012  Roel Huybrechts
 # All rights reserved.
 
+"""
+Module that provides a few useful classes for use in plugins or the server.
+"""
+
 from twisted.internet import threads
 
 import urllib2
 import urlparse
 
 class ExtRequest(urllib2.Request):
+    """
+    Class that extends the default urllib2.Request.
+    """
     method = None
 
     def set_method(self, method):
@@ -26,7 +33,19 @@ class ExtRequest(urllib2.Request):
             return self.method
 
 class RESTConnection(object):
+    """
+    Class that defines a REST connection.
+    """
     def __init__(self, base_url, timeout=40, username=None, password=None, authHandler=None):
+        """
+        Initialisation. Username, password and authHandler are optional, but depend on each other.
+
+        @param   base_url (str)   The base URL to connect to.
+        @param   timeout (int)    The timeout of the connection, in seconds. Defaults to 40.
+        @param   username (str)   The username to log in. Optional.
+        @param   password (str)   The password to log in. Optional.
+        @param   authHandler      A urllib2 authenication handler object. Optional.
+        """
         self.base_url = base_url
         self.timeout = float(timeout)
         self.username = username
@@ -49,26 +68,74 @@ class RESTConnection(object):
         self.path = path
 
     def request_get(self, resource, cb=None, headers={}):
+        """
+        Perform a GET request.
+
+        @param   resource (str)   The resource to call.
+        @param   cb (method)      A callback method to call when to request is done. Optional.
+                                    This method should take one argument which will be the result of the request.
+        @param   headers (dict)   Additional header to send with the request. Optional.
+        """
         self.request(cb, resource, "get", headers=headers)
 
     def request_delete(self, resource, cb=None, headers={}):
+        """
+        Perform a DELETE request.
+
+        @param   resource (str)   The resource to call.
+        @param   cb (method)      A callback method to call when to request is done. Optional.
+                                    This method should take one argument which will be the result of the request.
+        @param   headers (dict)   Additional header to send with the request. Optional.
+        """
         self.request(cb, resource, "delete", headers=headers)
 
     def request_head(self, resource, cb=None, headers={}):
+        """
+        Perform a HEAD request.
+
+        @param   resource (str)   The resource to call.
+        @param   cb (method)      A callback method to call when to request is done. Optional.
+                                    This method should take one argument which will be the result of the request.
+        @param   headers (dict)   Additional header to send with the request. Optional.
+        """
         self.request(cb, resource, "head", headers=headers)
 
     def request_post(self, resource, cb=None, body=None, headers={}):
+        """
+        Perform a POST request.
+
+        @param   resource (str)   The resource to call.
+        @param   cb (method)      A callback method to call when to request is done. Optional.
+                                    This method should take one argument which will be the result of the request.
+        @param   body (str)       The data to send with the request. Optional.
+        @param   headers (dict)   Additional header to send with the request. Optional.
+        """
         self.request(cb, resource, "post", body=body, headers=headers)
 
     def request_put(self, resource, cb=None, body=None, headers={}):
+        """
+        Perform a PUT request.
+
+        @param   resource (str)   The resource to call.
+        @param   cb (method)      A callback method to call when to request is done. Optional.
+                                    This method should take one argument which will be the result of the request.
+        @param   body (str)       The data to send with the request. Optional.
+        @param   headers (dict)   Additional header to send with the request. Optional.
+        """
         self.request(cb, resource, "put", body=body, headers=headers)
 
     def request(self, callback, resource, method="get", body=None, headers={}):
+        """
+        Wrapper to perform a request with a callback. Should not be called directly.
+        """
         d = threads.deferToThread(self.__request, resource, method, body, headers)
         if callback != None:
             d.addCallback(callback)
 
     def __request(self, resource, method="get", body=None, headers={}):
+        """
+        Perform the actual HTTP request. Should not be called directly.
+        """
         if resource.startswith('/'):
             req = ExtRequest(self.base_url+resource)
         else:
