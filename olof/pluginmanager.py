0b8da6fc1c296c75850024744b8593976a6d890b
Roel Huybrechts <mail@rulus.be>
2012-03-25 17:05:09 +0200

    Updates to pluginmanager.

--- olof/pluginmanager.py
+++ olof/pluginmanager.py
@@ -14,6 +14,11 @@ import os
 import sys
 import traceback
 
+import pyinotify
+from pyinotify import WatchManager, Notifier, ThreadedNotifier, EventsCodes, ProcessEvent
+
+from twisted.internet import reactor
+
 class PluginManager(object):
     """
     Class that represents the plugin manager.
@@ -28,15 +33,36 @@ class PluginManager(object):
 
         self.plugin_dirs = ['olof/plugins']
 
-        self.plugins = []
+        self.plugins = {}
         self.plugins_with_errors = {}
 
-        self.loadPlugins()
+        self.loadAllPlugins()
 
-    def loadPlugin(self, filename):
-        name = os.path.basename(filename)[:-3]
+        wm = WatchManager()
+        wm.add_watch('olof/plugins/', pyinotify.ALL_EVENTS)
+        self.inotifier = ThreadedNotifier(wm, self.processINotify)
+        self.inotifier.start()
+
+    def processINotify(self, event):
+        """
+        Process an INotify event, unloading and reloading plugins when applicable.
+        """
+        if not event.name.startswith('.') and event.name.endswith('.py'):
+            if event.mask == pyinotify.IN_CLOSE_WRITE:
+                self.unloadPlugin(event.name.rstrip('.py'))
+                self.loadPlugin(event.pathname)
+            elif event.mask == pyinotify.IN_DELETE:
+                self.unloadPlugin(event.name.rstrip('.py'))
+
+    def loadPlugin(self, path):
+        """
+        Load a plugin.
+
+        @param   path (str)   Path of the Python file of the plugin.
+        """
+        name = os.path.basename(path)[:-3]
         try:
-            plugin = imp.load_source(name, filename).Plugin(self.server)
+            plugin = imp.load_source(name, path).Plugin(self.server)
             if not plugin.isEnabled():
                 return
             plugin.filename = name
@@ -45,11 +71,11 @@ class PluginManager(object):
             self.server.output("Error while loading plugin %s: %s" % (name, e), sys.stderr)
         else:
             self.server.output("Loaded plugin: %s" % name)
-            self.plugins.append(plugin)
+            self.plugins[name] = plugin
 
-    def loadPlugins(self):
+    def loadAllPlugins(self):
         """
-        Load the plugins. Called automatically on initialisation.
+        Load all the plugins. Called automatically on initialisation.
         """
         home = os.getcwd()
 
@@ -62,14 +88,37 @@ class PluginManager(object):
         """
         Unload the plugin manager and all plugins.
         """
-        self.unloadPlugins()
+        self.inotifier.stop()
+        self.unloadAllPlugins()
 
-    def unloadPlugins(self):
+    def unloadAllPlugins(self):
         """
         Unload all the plugins.
         """
-        for p in self.plugins:
+        for p in self.plugins.values():
             p.unload()
+            del(p)
+
+    def unloadPlugin(self, name):
+        """
+        Unload the plugin with the given name.
+
+        @param   name (str)   Name of the plugin to unload. This is the filename, without the trailing '.py'.
+        """
+        p = self.getPlugin(name)
+        if p != None:
+            self.server.output('Unloaded plugin: %s' % p.filename)
+            p.unload()
+            del(self.plugins[name])
+
+    def getPlugin(self, name):
+        """
+        Get the plugin with the given name.
+
+        @param    name (str)           Name of the plugin. This is the filename, without the trailing '.py'.
+        @return   (olof.core.Plugin)   The Plugin with given name, None if none exists with such a name.
+        """
+        return self.plugins.get(name, None)
 
     def getPlugins(self):
         """
@@ -77,4 +126,4 @@ class PluginManager(object):
 
         @return   (list(olof.core.Plugin))   A List of all loaded plugins
         """
-        return self.plugins
+        return self.plugins.values()
