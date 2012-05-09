e6ff7f000c12be2af8452373aa85733480d9d222
Roel Huybrechts <mail@rulus.be>
2012-05-09 16:05:19 +0200

    Make it work without the Git stuff too.

--- olof/plugins/dashboard.py
+++ olof/plugins/dashboard.py
@@ -21,7 +21,6 @@ from twisted.web.static import File
 from zope.interface import implements
 
 import datetime
-import git
 import math
 import os
 import re
@@ -244,8 +243,10 @@ class ContentResource(resource.Resource):
 
         @return   (str)   HTML representation of the footer of the webpage.
         """
-        html = '<div id="footer"><p>Gyrid Server version <span title="%s">%s</span>.</p>' % (
-            self.plugin.git_commit, time.strftime('%Y-%m-%d', time.localtime(self.plugin.git_date)))
+        html = '<div id="footer">'
+        if self.plugin.git_commit != None and self.plugin.git_date != None:
+            html = '<p>Gyrid Server version <span title="%s">%s</span>.</p>' % (
+                self.plugin.git_commit, time.strftime('%Y-%m-%d', time.localtime(self.plugin.git_date)))
         html += '<p>&#169; 2011-2012 Universiteit Gent, Roel Huybrechts. '
         html += '<br>Icons by <a href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a>.</p>'
         html += '</div>'
@@ -388,10 +389,15 @@ class Plugin(olof.core.Plugin):
         except:
             self.cpuCount = 1
 
-        repo = git.Repo('.')
-        commit = repo.commits(repo.active_branch)[0]
-        self.git_commit = commit.id
-        self.git_date = int(time.strftime('%s', commit.committed_date))
+        try:
+            import git
+            repo = git.Repo('.')
+            commit = repo.commits(repo.active_branch)[0]
+            self.git_commit = commit.id
+            self.git_date = int(time.strftime('%s', commit.committed_date))
+        except:
+            self.git_commit = None
+            self.git_date = None
 
         self.parseMVNumbers()
 
